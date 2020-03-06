bef9d1164e1a0ee1bfad29a92752b38b973a224c
Yurii Mykhaletskyi <v-ymykhaletskyi@vrbo.com>
2020-03-06 17:51:04 +0200

    + Payment ticket generation

---
+++ src/main/java/com/drofff/palindrome/type/Payment.java
@@ -0,0 +1,78 @@
+package com.drofff.palindrome.type;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+
+import com.drofff.palindrome.document.ViolationType;
+import com.drofff.palindrome.enums.Currency;
+
+public class Payment {
+
+	@NotNull(message = "Amount is required")
+	@Min(value = 1, message = "Amount value should be greater than 0")
+	private Long amount;
+
+	@NotNull(message = "Currency is required")
+	private Currency currency;
+
+	@NotNull(message = "Payment description is missing")
+	private String description;
+
+	@NotNull(message = "Payment token is required")
+	private String paymentToken;
+
+	public Long getAmount() {
+		return amount;
+	}
+
+	public void setAmount(Long amount) {
+		this.amount = amount;
+	}
+
+	public Currency getCurrency() {
+		return currency;
+	}
+
+	public void setCurrency(Currency currency) {
+		this.currency = currency;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getPaymentToken() {
+		return paymentToken;
+	}
+
+	public void setPaymentToken(String paymentToken) {
+		this.paymentToken = paymentToken;
+	}
+
+	public static class Builder {
+
+		private Payment payment = new Payment();
+
+		public Builder forViolationType(ViolationType violationType) {
+			payment.amount = violationType.getFee().getAmount();
+			payment.currency = violationType.getFee().getCurrency();
+			payment.description = violationType.getName();
+			return this;
+		}
+
+		public Builder withPaymentToken(String token) {
+			payment.paymentToken = token;
+			return this;
+		}
+
+		public Payment build() {
+			return payment;
+		}
+
+	}
+
+}
